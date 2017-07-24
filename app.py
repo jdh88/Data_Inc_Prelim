dc12cc2471b6cd511f01255ac000deaec90fc63b
jdh88 <jonathan@Jonathans-MBP.fios-router.home>
2017-07-24 04:42:46 -0400

    Now we try with graphs

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+@app.route('/<string:page_name>/')
+def static_page(page_name):
+    return render_template('%s.html' % page_name)
 
 if __name__ == '__main__':
-            app.run()
+    app.run()
+
+#from flask import Flask
+#app = Flask(__name__)
+#
+#@app.route('/')
+#def hello():
+#    return "Hello World!"
+#
+#if __name__ == '__main__':
+#            app.run()
