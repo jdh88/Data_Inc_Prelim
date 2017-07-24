62920f010f09d4b1403f2d5f5c39c3a9c1e8ee72
jdh88 <jonathan@Jonathans-MBP.fios-router.home>
2017-07-24 09:00:47 -0400

    Hopefully added a second page

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/<string:page_name>/')
-def static_page(page_name):
-    return render_template('%s.html' % page_name)
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/plot1')
+def plot1():
+    return renter_template('frequency_comparison_graph.html')
 
 if __name__ == '__main__':
     app.run()
 
-#from flask import Flask
+
+#from flask import Flask, render_template
+#
 #app = Flask(__name__)
 #
-#@app.route('/')
-#def hello():
-#    return "Hello World!"
+#@app.route('/<string:page_name>/')
+#def static_page(page_name):
+#    return render_template('%s.html' % page_name)
 #
 #if __name__ == '__main__':
-#            app.run()
+#    app.run()
+#
+
+# This one works
+
