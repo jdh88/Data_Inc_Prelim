0380d59d0de2ff3d21f20e19b7bd041c9c062cd6
jdh88 <johirokawa@gmail.com>
2017-08-07 15:19:38 -0400

    Started to seperate out some of the layout from the rest of the page code

--- app.py
+++ app.py
@@ -7,13 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-   # user = {'nickname': 'Jonathan'}
-   # return render_template('index.html', title='Home', user=user)
-   return render_template('index.html')
+    # user = {'nickname': 'Jonathan'}
+    # return render_template('index.html', title='Home', user=user)
+    return render_template('index.html')
+
+@app.route('/aims')
+def aims():
+    return render_template('aims.html')
 
 
 @app.route('/graphs/<path:path>')
 def send_graph(path):
+    """
+    This function will render any html file I put in the graphs folder
+    """
     return send_from_directory('graphs', path)
 
 if __name__ == '__main__':
